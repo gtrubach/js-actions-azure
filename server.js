c44fc91f88cd347a22e7707a94cd19ac46539e82
Gennady Trubach <gennady.trubach@gmail.com>
2020-04-12 13:46:47 +0300

    Add files

---
+++ server.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/weatherforecast', (req, res) => {
+    res.json([{
+        "date": "2019-07-16T19:04:05.7257911-06:00",
+        "temperatureC": 52,
+        "temperatureF": 125,
+        "summary": "Mild"
+    }, {
+        "date": "2019-07-17T19:04:05.7258461-06:00",
+        "temperatureC": 36,
+        "temperatureF": 96,
+        "summary": "Warm"
+    }, {
+        "date": "2019-07-18T19:04:05.7258467-06:00",
+        "temperatureC": 39,
+        "temperatureF": 102,
+        "summary": "Cool"
+    }, {
+        "date": "2019-07-19T19:04:05.7258471-06:00",
+        "temperatureC": 10,
+        "temperatureF": 49,
+        "summary": "Bracing"
+    }, {
+        "date": "2019-07-20T19:04:05.7258474-06:00",
+        "temperatureC": -1,
+        "temperatureF": 31,
+        "summary": "Chilly"
+    }]);
+});
+
+var server = app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+module.exports = server;
